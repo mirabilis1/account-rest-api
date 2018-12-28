5f5fb2883fc08400f70a1d404c65c0d87c9d8dd7
Bobby Das <das.sambeet@gmail.com>
2018-12-28 13:00:14 +0000

    committing passing tests

--- src/main/java/com/mirabilis/account/service/AccountServiceImpl.java
+++ src/main/java/com/mirabilis/account/service/AccountServiceImpl.java
@@ -1,9 +1,11 @@
 package com.mirabilis.account.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mirabilis.account.dao.AccountDao;
 import com.mirabilis.account.model.Account;
@@ -19,27 +21,31 @@ public class AccountServiceImpl implements AccountService {
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Account> getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		return (List<Account>) accountDao.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Account getAccountById(Long id) {
-		// TODO Auto-generated method stub
+		Optional<Account> optional = accountDao.findById(id);
+		if (optional.isPresent()) {
+			return optional.get();
+		}
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public Account saveAccount(Account account) {
-		// TODO Auto-generated method stub
-		return null;
+		return accountDao.save(account);
 	}
 
 	@Override
+	@Transactional
 	public void deleteAccount(Account Account) {
-		// TODO Auto-generated method stub
-		
+		accountDao.delete(Account);
 	}
 
 }
