7d207f79f9f7c1557769450fc67a323e6db8de5d
Bobby Das <das.sambeet@gmail.com>
2018-12-28 11:26:01 +0000

    First commit

---
+++ src/main/java/com/mirabilis/account/AccountRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.mirabilis.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountRestApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/mirabilis/account/AccountRestApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mirabilis.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
